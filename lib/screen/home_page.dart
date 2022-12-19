d4b4871e3641ce62335e0a3a469d663012e50cbf
Mr.Nobody <dr.void@MrNobodys-MacBook-Pro.local>
2022-12-19 18:25:10 +0300

    review01

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+// import 'package:flutter/src/widgets/container.dart';
+// import 'package:flutter/src/widgets/framework.dart';
 
 //Image.network(src)
 class HomePage extends StatefulWidget {
